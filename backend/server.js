2ea18a4a6e21f0f6338cbebae1e679b97f3f6244
vasanthikondreddy <k.vasanthi882005@gmail.com>
2025-08-15 11:28:35 +0530

    chnaged port

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ mongoose
 
     init(server);
 
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
